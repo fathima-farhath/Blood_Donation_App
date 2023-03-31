48dbee237d8e9b13b329470664d3ad5ad8dac34d
Farhath <farhathali631@gmail.com>
2023-03-31 21:02:00 +0530

    Added the form inside the add.dart file

--- lib/proj1/add.dart
+++ lib/proj1/add.dart
@@ -10,6 +10,9 @@ class AddUser extends StatefulWidget {
 }
 
 class _AddUserState extends State<AddUser> {
+
+  final blood_group=['A+','A-','AB+','AB-','O+','O-','B+','B-'];
+  String ? Selected_group;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,12 +29,75 @@ class _AddUserState extends State<AddUser> {
         child: Column(
           // ignore: prefer_const_literals_to_create_immutables
           children: [
-            TextField(
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius:BorderRadius.circular(20),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.amber),
+                    borderRadius:BorderRadius.circular(20),
+                  ),
+                  labelText:"Donor's Name",
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 15.0,
+                  ),
+                  hintText: "Enter your name",
+                  hintStyle: TextStyle(
+                    color: Colors.black,
+                  ),
                 ),
-                label: Text("Donor name"),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: TextField(
+                keyboardType: TextInputType.phone,
+                maxLength: 10,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderRadius:BorderRadius.circular(20),
+                  ),
+                  labelText:"Phone Number",
+                  labelStyle: TextStyle(
+                    color: Colors.black,
+                    fontSize: 18.0,
+                  ),
+                  hintText: "Enter your phone number",
+                  hintStyle: TextStyle(
+                    color: Colors.black,
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: DropdownButtonFormField(
+                decoration: InputDecoration(label: Text(
+                  "Select the blood group"
+                )),
+                items: blood_group.map((e) => DropdownMenuItem(
+                child: Text(e),
+                value: e,
+                )).toList(), 
+              onChanged:(val){
+                Selected_group=val as String?;
+              }),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: ElevatedButton(onPressed: (){
+            
+              },style: ButtonStyle(
+                minimumSize: MaterialStatePropertyAll(Size(double.infinity,50 )),
+                backgroundColor: MaterialStatePropertyAll(Colors.red),
+                foregroundColor: MaterialStatePropertyAll(Colors.blue)
+              )
+              , child: Text("Submit",
+              style: TextStyle(
+                fontSize: 25.0,
+                color: Colors.white,
+              ),),
               ),
             )
           ],
